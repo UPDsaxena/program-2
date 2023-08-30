c448357edc59ebb924a06ac311022789420e9b8c
user <user@DESKTOP-3C631U4>
2023-08-30 12:13:52 +0530

    Add project files.

---
+++ program 2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            double sum = 0;
+            Console.WriteLine("Enter 5 Elements");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            double avg = sum / 5;
+
+            Console.WriteLine("Average is :" + avg);
+        }
+    }
+}
